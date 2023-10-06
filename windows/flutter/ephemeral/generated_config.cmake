782286d9822de0e544dda807a1cae573917cc282
Oluwaseyi Ogunjinmi <80404459+Oluwaseyieniola@users.noreply.github.com>
2023-10-06 22:24:20 +0100

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\USER\\Documents\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\USER\\Messenger-App-Backend-Firebase" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\USER\\Documents\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\USER\\Messenger-App-Backend-Firebase"
+  "FLUTTER_ROOT=C:\\Users\\USER\\Documents\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\USER\\Messenger-App-Backend-Firebase\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\USER\\Messenger-App-Backend-Firebase"
+  "FLUTTER_TARGET=C:\\Users\\USER\\Messenger-App-Backend-Firebase\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iZDk4NmM1ZWQyMGE2MmRjMzRiNzcxOGM1MGFiYzc4MmJlYWU0YzMzLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\USER\\Messenger-App-Backend-Firebase\\.dart_tool\\package_config.json"
+)
